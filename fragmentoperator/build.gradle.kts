ce21a9fa0ed5424caa7684478d7b8bbad0e3a8b7
hm1113 <liuhm1113@gmail.com>
2025-01-29 23:34:27 -0800

    Update to gradle 8.10

---
+++ fragmentoperator/build.gradle.kts
@@ -0,0 +1,57 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+    id("maven-publish")
+}
+
+group = "com.github.hm1113"
+version = "1.2.0"
+
+android {
+    namespace = "com.tubitv.fragmentoperator"
+    compileSdk = 35
+
+    defaultConfig {
+        minSdk = 24
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("release") {
+            afterEvaluate { // Ensures all components are available
+                from(components["release"]) // ✅ Use "release" for Android libraries
+            }
+            groupId = "com.github.hm1113"
+            artifactId = "hm.fragmentoperator"
+            version = "1.2.0"
+        }
+    }
+}
